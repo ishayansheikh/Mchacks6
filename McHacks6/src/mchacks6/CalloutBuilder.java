3bcea54f782302ead41283fbe157a1daa066e955
jianqiu chen <jianqiu@jianqiu.com>
2019-02-02 20:00:08 -0500

    callout added

---
+++ McHacks6/src/mchacks6/CalloutBuilder.java
@@ -0,0 +1,94 @@
+package mchacks6;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class CalloutBuilder {
+
+    private Callout callout = new Callout();
+
+    private CalloutBuilder() {
+    }
+
+    private CalloutBuilder(Callout callout) {
+        this.callout = callout;
+    }
+
+    public static CalloutBuilder create() {
+        return new CalloutBuilder();
+    }
+
+    public static CalloutBuilder create(Class<? extends Callout> clazz) {
+        try {
+            return new CalloutBuilder(clazz.getDeclaredConstructor().newInstance());
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public CalloutBuilder headPoint(double x, double y) {
+        callout.setHeadPoint(x, y);
+        return this;
+    }
+
+    public CalloutBuilder leaderLineToPoint(double x, double y) {
+        callout.setLeaderLineToPoint(x, y);
+        return this;
+    }
+
+    public CalloutBuilder endLeaderLineRight(double length) {
+        callout.setEndLeaderLineLength(length);
+        callout.setEndLeaderLineDirection(Callout.RIGHT);
+        return this;
+    }
+
+    public CalloutBuilder endLeaderLineRight() {
+        callout.setEndLeaderLineLength(75);
+        callout.setEndLeaderLineDirection(Callout.RIGHT);
+        return this;
+    }
+
+    public CalloutBuilder endLeaderLineLeft(double length) {
+        callout.setEndLeaderLineLength(length);
+        callout.setEndLeaderLineDirection(Callout.LEFT);
+        return this;
+    }
+
+    public CalloutBuilder endLeaderLineLeft() {
+        callout.setEndLeaderLineLength(75);
+        callout.setEndLeaderLineDirection(Callout.LEFT);
+        return this;
+    }
+
+    public CalloutBuilder endLeaderLineLength(double length) {
+        callout.setEndLeaderLineLength(length);
+        return this;
+    }
+
+    public CalloutBuilder endLeaderLineDirection(int direction) {
+        callout.setEndLeaderLineDirection(direction);
+        return this;
+    }
+
+    public CalloutBuilder mainTitle(String title) {
+        callout.setMainTitleText(title);
+        return this;
+    }
+
+    public CalloutBuilder subTitle(String title) {
+        callout.setSubTitleText(title);
+        return this;
+    }
+
+    public CalloutBuilder pause(long inMillis) {
+        callout.setPauseTime(inMillis);
+        return this;
+    }
+
+    public Callout build() {
+        callout.build();
+        return callout;
+    }
+}
--- McHacks6/src/mchacks6/McHacks6.java
+++ McHacks6/src/mchacks6/McHacks6.java
@@ -1,25 +1,15 @@
 package mchacks6;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.SequentialTransition;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 /**
  *
@@ -29,37 +19,99 @@ public class McHacks6 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setTitle("Callouts");
+        Pane root = new Pane();
+        int maxWidth = 1024;
 
-        StackPane root = new StackPane();
-        root.setStyle("-fx-background-color:#685343");
-        Image imgBackground = new Image("images/mountain.jpg");
-        ImageView ivBackground = new ImageView(imgBackground);
-        root.getChildren().add(ivBackground);
-        ivBackground.fitHeightProperty().bind(root.heightProperty());
-        ivBackground.fitWidthProperty().bind(root.widthProperty());
-        SequentialTransition calloutAnimation = new SequentialTransition();
-        calloutAnimation.setCycleCount(2);
-        calloutAnimation.setAutoReverse(true);
-        Circle head = new Circle(600, 550, 5); // center x, y, radius
-        head.setFill(Color.WHITE);
-        calloutAnimation.getChildren().add(buildHeadAnim(head));
-        calloutAnimation.play();
-        Scene scene = new Scene(root, 800, 650);
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, maxWidth, 720));
+
+
+        Image image = new Image("images/mountain.jpg", true);
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.fitWidthProperty().bind(primaryStage.widthProperty());
+
+        Callout callout1 = CalloutBuilder.create(CustomCallout.class)
+                .headPoint(600, 550)
+                .leaderLineToPoint(400, 300)
+                .endLeaderLineLeft()
+                .mainTitle("Your Colleague Stabbed")
+                .subTitle("Distance from snipper : 20m")
+                .pause(50000)
+                .build();
+
+        Callout callout2 = CalloutBuilder.create()
+                .headPoint(200, 550)
+                .leaderLineToPoint(400, 350.5)
+                .endLeaderLineRight()
+                .mainTitle("Your Colleague Shot")
+                .subTitle("Distance from snipper : 300m")
+                .pause(50000)
+                .build();
+
+        Callout callout3 = CalloutBuilder.create()
+                .headPoint(498, 386)
+                .leaderLineToPoint(375, 406)
+                .endLeaderLineLeft()
+                .mainTitle("Your Colleague Shot")
+                .subTitle("Distance from snipper : 2000m")
+                .pause(50000)
+                .build();
+
+        Callout callout4 = CalloutBuilder.create()
+                .headPoint(667, 400)
+                .leaderLineToPoint(695, 500)
+                .endLeaderLineRight()
+                .mainTitle("Smoke detected")
+                .subTitle("Distance from snipper : 200m")
+                .pause(50000)
+                .build();
+
+        root.getChildren().add(imageView);
+        Label instructions = new Label("Please Kill the snipper");
+        
+        instructions.setStyle("-fx-text-fill: white; " +
+                "-fx-background-color: linear-gradient(#f4ce42, #f4a941); " +
+                "-fx-effect: dropshadow(three-pass-box , rgba(0,0,0,0.6), 5, 0.0 , 0 , 1 );" +
+                "-fx-border-insets: 10;" +
+                "-fx-font-size: 20;" +
+                "-fx-label-padding: 5;");
+        root.getChildren().add(instructions);
+        root.getChildren().addAll(callout1, callout2, callout3, callout4);
+
+        // Animate 1-4 callouts to point out things in the picture
+        EventHandler<KeyEvent> calloutKeyHandler = keyEvent -> {
+            switch (keyEvent.getCharacter()) {
+                case "1":
+                    callout1.play();
+                    break;
+                case "2":
+                    callout2.play();
+                    break;
+                case "3":
+                    callout3.play();
+                    break;
+                case "4":
+                    callout4.play();
+                    break;
+                default:
+                    callout1.play();
+            }
+        };
+
+        root.addEventHandler(KeyEvent.KEY_TYPED, calloutKeyHandler);
+        root.requestFocus();
+
+        // Observe mouse clicked coordinates
+        root.setOnMouseClicked(mouseevent -> {
+            System.out.println(mouseevent);
+        });
+
         primaryStage.show();
+        
+        
     }
 
-    protected Animation buildHeadAnim(Node head) {
-        Circle headCircle = (Circle) head;
-        return new Timeline(
-                new KeyFrame(Duration.millis(1),
-                        new KeyValue(headCircle.visibleProperty(), true),
-                        new KeyValue(headCircle.radiusProperty(), 0)), // start value
-                new KeyFrame(Duration.millis(300),
-                        new KeyValue(headCircle.radiusProperty(), 5.0d)) // end value
-        );
-    }
     
     public static void main(String[] args) {
         launch(args);
